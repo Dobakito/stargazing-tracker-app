694025542ddaafefb514b9ccf2ba8ad7210ead80
michael dobak <mdobak44@icloud.com>
2021-09-01 16:19:23 -0500

    Added initial routes
    Added the login and signup routes to app.js and created the initial login and signup components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { Button, Icon } from 'semantic-ui-react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Routes/Login';
+import Signup from './Routes/Signup';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className='App'>
+        <Route path='/login' component={Login}>
+          Login
+        </Route>
+        <Route path='/signup' component={Signup}>
+          Signup
+        </Route>
+      </div>
+    </Router>
   );
 }
 
