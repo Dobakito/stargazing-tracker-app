a08688068c9401a95f3c641d594ab9b32f42d613
michael dobak <mdobak44@icloud.com>
2021-09-01 17:59:12 -0500

    Added component skeletons
    Added component skeletons to card, login, show, objects, and profile. deleted some unneccessary apps

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,25 @@
-import './App.css';
-import { Button, Icon } from 'semantic-ui-react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 import Login from './Routes/Login';
 import Signup from './Routes/Signup';
+import Nav from './Component/Nav';
+import Profile from './Routes/Profile';
+import Objects from './Routes/Objects';
+import Show from './Routes/Show';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Route path='/login' component={Login}>
-          Login
+    <div className='App'>
+      <Nav />
+      <Login />
+      <Switch>
+        <Route path='/login' component={Login} />
+        <Route path='/signup' component={Signup} />
+        <Route path='/profile' component={Profile} />
+        <Route exact path='/objects' component={Objects}>
+          <Route path='/objects/:id' component={Show} />
         </Route>
-        <Route path='/signup' component={Signup}>
-          Signup
-        </Route>
-      </div>
-    </Router>
+      </Switch>
+    </div>
   );
 }
 
---
+++ frontend/src/Component/Login.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import { Form } from 'semantic-ui-react';
+
+export default class Login extends Component {
+  render() {
+    return <Form></Form>;
+  }
+}
